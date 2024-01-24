425f457359110f67d7d6be9992b12ab090ec0cbd
anatolii <anatoliise@mail.ru>
2024-01-25 01:16:59 +0300

    test

---
+++ src/main/java/com/skypro/homeWorkList/HomeWorkListApplication.java
@@ -0,0 +1,15 @@
+package com.skypro.homeWorkList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWorkListApplication.class, args);
+	}
+
+
+
+}
